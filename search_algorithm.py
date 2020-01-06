86cf2d6e6ab76d8ada3d0e21053d97c4babdb683
sunghyub <sunghyub@andrew.cmu.edu>
2020-01-07 07:56:03 +0900

    most recent

---
+++ search_algorithm.py
@@ -0,0 +1,13 @@
+'''
+Search Algorithms
+
+two most popular search - linar search & binary search
+
+Linear - O(n) - just start from beginning to end and if not exist, return -1
+
+binary - start searching from the middle of the list/array
+if match, return the index
+if not match, check if its greater than or less than and repeat
+time complexity O(logn)
+-> every incorrect guess, it cuts down the list of elements in half 
+'''
---
+++ sorting_algorithms.py
@@ -0,0 +1,152 @@
+'''
+sorting algorithms
+
+first of all, python has a builtin sort
+
+'''
+
+list = [1,5,76,25,78,3,17,85]
+list.sort()
+print(list)
+
+'''
+this is a destructive sort, assuming its not the most efficient one, but 
+should work for coding interviews
+
+common ones are bubble sort, insertion sort, selection sort, quick sort, 
+merge sort, heap sort, counting sort
+
+bubble sort -> compare first two element. If they out of order, swap
+do this n-1, then n-2 (except for the last element), ... 
+so n(n-1)/2 so O(n^2) time complex, but O(1) space complexity 
+
+Selection sort -> search smallest array, swap with first, repeat this 
+very intuitive 
+
+Insertion sort -> it's like playing big 2. U take out a card and place it 
+at the right position. better than bubble, selection sort 
+Go from low to high and insert them at the right position. start @ index 1
+
+quick sort -> we have a pivot element (rightmost or something like that)
+whatever is less goes left, whatever greater goes right
+keep repeating this nlogn or n^2 complexity 
+
+Merge sort -> most popular sorting algorithm i guess
+nlogn run time, divide and conquer 
+
+'''
+  
+# Merges two subarrays of arr[]. 
+# First subarray is arr[l..m] 
+# Second subarray is arr[m+1..r] 
+def merge(arr, l, m, r): 
+    n1 = m - l + 1
+    n2 = r- m 
+  
+    # create temp arrays 
+    L = [0] * (n1) 
+    R = [0] * (n2) 
+  
+    # Copy data to temp arrays L[] and R[] 
+    for i in range(0 , n1): 
+        L[i] = arr[l + i] 
+  
+    for j in range(0 , n2): 
+        R[j] = arr[m + 1 + j] 
+  
+    # Merge the temp arrays back into arr[l..r] 
+    i = 0     # Initial index of first subarray 
+    j = 0     # Initial index of second subarray 
+    k = l     # Initial index of merged subarray 
+  
+    while i < n1 and j < n2 : 
+        if L[i] <= R[j]: 
+            arr[k] = L[i] 
+            i += 1
+        else: 
+            arr[k] = R[j] 
+            j += 1
+        k += 1
+  
+    # Copy the remaining elements of L[], if there 
+    # are any 
+    while i < n1: 
+        arr[k] = L[i] 
+        i += 1
+        k += 1
+  
+    # Copy the remaining elements of R[], if there 
+    # are any 
+    while j < n2: 
+        arr[k] = R[j] 
+        j += 1
+        k += 1
+  
+# l is for left index and r is right index of the 
+# sub-array of arr to be sorted 
+def mergeSort(arr,l,r): 
+    if l < r: 
+  
+        # Same as (l+r)/2, but avoids overflow for 
+        # large l and h 
+        m = (l+(r-1))/2
+  
+        # Sort first and second halves 
+        mergeSort(arr, l, m) 
+        mergeSort(arr, m+1, r) 
+        merge(arr, l, m, r) 
+  
+#space complexity of O(n), best to sort linked list 
+
+''' 
+
+Heap sort - heap is a tree based datastructure 
+
+put the array into a heap datastructure, and then sort 
+
+'''
+
+# To heapify subtree rooted at index i. 
+# n is size of heap 
+def heapify(arr, n, i): 
+    largest = i  # Initialize largest as root 
+    l = 2 * i + 1     # left = 2*i + 1 
+    r = 2 * i + 2     # right = 2*i + 2 
+  
+    # See if left child of root exists and is 
+    # greater than root 
+    if l < n and arr[i] < arr[l]: 
+        largest = l 
+  
+    # See if right child of root exists and is 
+    # greater than root 
+    if r < n and arr[largest] < arr[r]: 
+        largest = r 
+  
+    # Change root, if needed 
+    if largest != i: 
+        arr[i],arr[largest] = arr[largest],arr[i]  # swap 
+  
+        # Heapify the root. 
+        heapify(arr, n, largest) 
+  
+# The main function to sort an array of given size 
+def heapSort(arr): 
+    n = len(arr) 
+  
+    # Build a maxheap. 
+    for i in range(n, -1, -1): 
+        heapify(arr, n, i) 
+  
+    # One by one extract elements 
+    for i in range(n-1, 0, -1): 
+        arr[i], arr[0] = arr[0], arr[i]   # swap 
+        heapify(arr, i, 0)
+
+
+
+
+
+
+
+
